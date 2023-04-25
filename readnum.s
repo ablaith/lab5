1fc9c2fd536231f67b7742401fd27526ca6fddca
ablaith <abusail@wwu.edu>
2023-04-25 11:36:14 -0700

    removed 2 rdx calls

--- readnum.s
+++ readnum.s
@@ -13,14 +13,14 @@ main:
     mov $0, %rax    # 0 is system call id for read?
     mov $0, %rdi    # 0 is fd for stdin
     mov $num, %rsi  # load buffer address into rsi
-    mov $100 %rdx   # move size of buffer into rdx
+    #mov $100 %rdx   # move size of buffer into rdx
     syscall
 
     #write the message 2
     mov $1, %rax        # 1 is system call id for write
     mov $1, %rdi        # 1 is fd for stdout
     mov $message2, %rsi      # address of message
-    mov $100, %rdx       # write 18 bytes
+    #mov $100, %rdx       # write 18 bytes
     syscall
 
     # exit(42)
