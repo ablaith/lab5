4d03fd426a5e8b3aad1088046f6d595ef89623df
ablaith <abusail@wwu.edu>
2023-04-25 11:17:42 -0700

    more monke

--- readnum.s
+++ readnum.s
@@ -14,17 +14,23 @@ main:
     mov $0, %rdi    # 0 is fd for stdin
     mov $num, %rsi  # load buffer address into rsi
     mov $100 %rdx   # move size of buffer into rdx
+    syscall
 
-    #write the number
+    #write the message 2
     mov $1, %rax        # 1 is system call id for write
     mov $1, %rdi        # 1 is fd for stdout
     mov $message2, %rsi      # address of message
     mov $18, %rdx       # write 18 bytes
     syscall
 
+    #write the num
+    #mov $1, %rax        # 1 is system call id for write
+    #mov $1, %rdi        # 1 is fd for stdout
+    #mov $num, %rsi      # address of message
 
 
     .data
 message1: .ascii "Please enter a number: "
 message2: .ascii "You have entered: "
+newline: .ascii "\n"
 num: .zero 100
