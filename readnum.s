f7a5b16b60c9b4e17e2d01e4b4f007bf1c22b100
ablaith <abusail@wwu.edu>
2023-04-25 10:36:47 -0700

    testing printing

--- readnum.s
+++ readnum.s
@@ -1,1 +1,17 @@
+    .global main
 
+    .text
+main:
+    #write "enter num" message
+    mov $1, %rax        # 1 is system call id for write
+    mov $1, %rdi        # 1 is fd for stdout
+    mov $message, %rsi      # address of message
+    mov $21, %rdx       # write 21 bytes
+    syscall
+
+
+
+
+
+    .data
+message: .ascii "Please enter a number\n"
